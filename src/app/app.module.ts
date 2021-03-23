777ad6fd948d8bfc52d1526d5078fbe04ed5bbcd
d.zharikov <Dima.Zharikov@gmail.com>
2021-03-23 17:16:10 +0300

    create apps. Added response on server. Create types.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainPagesComponent} from './main-pages/main-pages.component';
+import {HttpClientModule} from '@angular/common/http';
+import {SliderService} from './main-pages/shared/slider.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [SliderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
